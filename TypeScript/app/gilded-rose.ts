4fb63953b38df7f1b01f5f611b4f0b7ffeba6b5a
Jing Wang <jing.wang@lesmills.com>
2023-10-07 22:45:45 +1300

    refactor: refactor updateQuality

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,64 +1,67 @@
 import { NormalItemQualityChangePerDay, SpecialItemName } from "./consts";
 import { Item } from "./item";
+import { updateSingleItemQuality } from "./utils";
 
 export class GildedRose {
-  items: Array<Item>;
+  items: Array<Item>
 
   constructor(items = [] as Array<Item>) {
-    this.items = items;
+    this.items = items
+  }
+
+  updateSingleItem(item: Item): Item {
+    if (item.name === SpecialItemName.Sulfuras) {
+      //"Sulfuras" is a legendary item and as such its Quality is 80 and it never alters
+      return item
+    }
+
+    const sellInPassed = item.sellIn <= 0
+    item.sellIn--
+
+    if (item.name === SpecialItemName.Conjured) {
+      const conjuredDecresedNum = sellInPassed
+        ? NormalItemQualityChangePerDay * 2 * 2
+        : NormalItemQualityChangePerDay * 2
+
+      updateSingleItemQuality(item, conjuredDecresedNum * -1)
+
+      return item
+    }
+
+    if (item.name === SpecialItemName.AgedBrie) {
+      const agedIncreasedNum = sellInPassed
+        ? NormalItemQualityChangePerDay * 2
+        : NormalItemQualityChangePerDay
+      updateSingleItemQuality(item, agedIncreasedNum)
+      return item
+    }
+
+    if (item.name === SpecialItemName.BackstagePasses) {
+      if (sellInPassed) {
+        item.quality = 0
+      } else {
+        const passesIncreasedNum = item.sellIn < 5 ? 3
+          : item.sellIn < 10 ? 2
+            : NormalItemQualityChangePerDay
+        updateSingleItemQuality(item, passesIncreasedNum)
+      }
+
+      return item
+    }
+
+    updateSingleItemQuality(
+      item,
+      sellInPassed
+        ? (NormalItemQualityChangePerDay * 2 * -1)
+        : (NormalItemQualityChangePerDay * -1))
+
+    return item
+
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name !== SpecialItemName.AgedBrie && this.items[i].name !== SpecialItemName.BackstagePasses) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name === SpecialItemName.Conjured) {
-            const newQuality = this.items[i].quality - (NormalItemQualityChangePerDay * 2)
-            this.items[i].quality = newQuality < 0 ? 0 : newQuality
-          } else if (this.items[i].name !== SpecialItemName.Sulfuras) {
-
-            this.items[i].quality = this.items[i].quality - NormalItemQualityChangePerDay
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + NormalItemQualityChangePerDay
-          if (this.items[i].name === SpecialItemName.BackstagePasses) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + NormalItemQualityChangePerDay
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + NormalItemQualityChangePerDay
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name !== SpecialItemName.Sulfuras) {
-        this.items[i].sellIn = this.items[i].sellIn - NormalItemQualityChangePerDay
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name !== SpecialItemName.AgedBrie) {
-          if (this.items[i].name !== SpecialItemName.BackstagePasses) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name !== SpecialItemName.Sulfuras) {
-                this.items[i].quality = this.items[i].quality - NormalItemQualityChangePerDay
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + NormalItemQualityChangePerDay
-          }
-        }
-      }
-    }
-
-    return this.items;
+    return this.items.map((item) => {
+      return this.updateSingleItem(item)
+    })
   }
 }
--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -8,19 +8,80 @@ describe('Conjured item', () => {
     const itemInADay = conjuredItem.updateQuality();
     expect(itemInADay[0].quality).toBe(3);
   });
+
+  it('should degrade in Quality twice as fast as normal items after sell by date has passed', () => {
+    const conjuredItem = new GildedRose([new Item(SpecialItemName.Conjured, 0, 5)]);
+    const itemInADay = conjuredItem.updateQuality();
+    expect(itemInADay[0].quality).toBe(1);
+  });
 });
 
 describe('general', () => {
   const generalItemName = '+5 Dexterity Vest'
+
+  it('the sellin of an item should decreade everyday', () => {
+    const generalItem = new GildedRose([new Item(generalItemName, 1, 0)]);
+    const items = generalItem.updateQuality();
+    expect(items[0].sellIn).toBe(0);
+  });
+
   it('the quality of an item should never be negative', () => {
     const generalItem = new GildedRose([new Item(generalItemName, 1, 0)]);
     const items = generalItem.updateQuality();
     expect(items[0].quality).toBe(0);
   });
 
-  it('the quality of an item should decrese', () => {
+  it('the quality of an item should decrese everyday', () => {
     const generalItem = new GildedRose([new Item(generalItemName, 1, 1)]);
     const items = generalItem.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
+  it('Once the sell by date has passed, Quality should degrades twice as fast', () => {
+    const generalItem = new GildedRose([new Item(generalItemName, 0, 3)]);
+    const items = generalItem.updateQuality();
+    expect(items[0].quality).toBe(1);
+  });
+
+  it('the quality of an item should never be above 50', () => {
+    const agedItem = new GildedRose([new Item(SpecialItemName.AgedBrie, 1, 50)]);
+    const items = agedItem.updateQuality();
+    expect(items[0].quality).toBe(50);
+  });
+})
+
+describe('Aged item', () => {
+  it('the quality of an aged item should increase everyday', () => {
+    const agedItem = new GildedRose([new Item(SpecialItemName.AgedBrie, 1, 20)]);
+    const items = agedItem.updateQuality();
+    expect(items[0].quality).toBe(21);
+  });
+})
+
+describe('Sulfuras item', () => {
+  it('sulfuras item should have quality as 80', () => {
+    const sulfurasItem = new GildedRose([new Item(SpecialItemName.Sulfuras, 1, 80)]);
+    const items = sulfurasItem.updateQuality();
+    expect(items[0].quality).toBe(80);
+  });
+})
+
+describe('Backstage passes item', () => {
+  it('Backstage passes should increase by 2 when there are 10 days or less', () => {
+    const backstagePassesItem = new GildedRose([new Item(SpecialItemName.BackstagePasses, 8, 8)]);
+    const items = backstagePassesItem.updateQuality();
+    expect(items[0].quality).toBe(10);
+  });
+
+  it('Backstage passes should increase by 3 when there are 5 days or less', () => {
+    const backstagePassesItem = new GildedRose([new Item(SpecialItemName.BackstagePasses, 3, 8)]);
+    const items = backstagePassesItem.updateQuality();
+    expect(items[0].quality).toBe(11);
+  });
+
+  it('Backstage passes quality should drops to 0 after the concert', () => {
+    const backstagePassesItem = new GildedRose([new Item(SpecialItemName.BackstagePasses, 0, 8)]);
+    const items = backstagePassesItem.updateQuality();
     expect(items[0].quality).toBe(0);
   });
 })
